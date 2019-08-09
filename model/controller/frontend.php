4768c41ad45b18fd5b0d38cfb4838edceee52c0d
Steven <47388330+stevenreno@users.noreply.github.com>
2019-08-09 11:58:22 +0200

    Delete frontend.php

--- model/controller/frontend.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('model/gestionChapitre.php');
-
-function listeChapitre()
-{
-    $gestionChapitre = new gestionChapitre();
-    $chapitres = $gestionChapitre->getPosts();
-    require_once('view/vueChapitre.php');
-}
