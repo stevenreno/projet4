9de35beda50ff10aae4b09771d419a1ab57de022
Steven <47388330+stevenreno@users.noreply.github.com>
2019-08-09 14:18:45 +0200

    Add files via upload

--- controller/backend.php
+++ controller/backend.php
@@ -2,9 +2,19 @@
 require_once('model/gestionChapitre.php');
 require_once('view/ecrireChapitre.php');
 
-
-function postUnChapitre($titre, $contenu, $numero_chapitre, $en_ligne)
-{
-    $gestionChapitre = new gestionChapitre();
-    $nouveauxChapitre = $gestionChapitre->createPost($titre, $contenu, $numero_chapitre, $en_ligne);
+class Backend {
+    private $titre;
+    private $contenu;
+    private $numero_chapitre;
+    private $en_ligne;
+    
+    public function postUnChapitre($titre, $contenu, $numero_chapitre, $en_ligne)
+    {
+        $this -> titre = $titre;
+        $this-> contenu = $contenu;
+        $this-> numero_chapitre = $numero_chapitre;
+        $this-> en_ligne=$en_ligne;
+        $gestionChapitre = new gestionChapitre();
+        $nouveauxChapitre = $gestionChapitre->createPost($titre, $contenu, $numero_chapitre, $en_ligne);
+    }
 }
--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php
 require_once('controller/frontend.php');
 require_once('controller/backend.php');
+$backend = new Backend();
+$frontend = new Frontend();
 
 
 if (isset($_GET['action'])) {
     if ($_GET['action'] == 'submitPost') {
         if (!empty($_POST['titre']) && !empty($_POST['contenu']) && !empty($_POST['numero_chapitre'])&& !empty($_POST['en_ligne'])) {
-            postUnChapitre($_POST['titre'], $_POST['contenu'],$_POST['numero_chapitre'],  $_POST['en_ligne']);
+            $backend->postUnChapitre($_POST['titre'], $_POST['contenu'],$_POST['numero_chapitre'],  $_POST['en_ligne']);
         } else {
             print'Contenu vide !';
         }
     }
     if ($_GET['action'] == 'submitComment') {
         if (!empty($_POST['auteurs']) && !empty($_POST['titre']) && !empty($_POST['contenu_commentaire'])) {
-            postUnCommentaire($_POST['auteurs'], $_POST['titre'],$_POST['contenu_commentaire']);
+            $frontend->postUnCommentaire($_POST['auteurs'], $_POST['titre'],$_POST['contenu_commentaire']);
         } else {
             print'Contenu vide !';
         }
@@ -21,6 +23,6 @@ if (isset($_GET['action'])) {
 }
 else
 {
-    listeChapitre();
-    listeCommentaire();
+    $frontend->listeChapitre();
+    $frontend->listeCommentaire();
 };
